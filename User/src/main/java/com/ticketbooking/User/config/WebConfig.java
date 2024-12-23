12a923f19701fadeb14b6b9d73c4bb3d5dbc1ae9
Harish Vodeyavargam Anand <harish.va1910@gmail.com>
2024-12-22 19:51:40 -0500

    Update WebConfig.java

--- User/src/main/java/com/ticketbooking/User/config/WebConfig.java
+++ User/src/main/java/com/ticketbooking/User/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost", "http://localhost:5173")
+                .allowedOrigins("https://eventix.website")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type")
                 .allowCredentials(true);
